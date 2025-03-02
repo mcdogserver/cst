ce3cce5f458b824e006dcad123c07793d05799a9
worldcctv48 <146924973+mcdogserver@users.noreply.github.com>
2025-03-02 10:10:25 +0800

    Add files via upload

---
+++ class/config.php
@@ -0,0 +1,16 @@
+<?php
+define('SKIN_LANGUAGE', 'skins_zh-CN');
+
+define('DB_HOST', 'ld.frp.one');
+define('DB_PORT', '25585');
+define('DB_NAME', 'mysql');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+
+define('WEB_STYLE_DARK', true);
+
+define('STEAM_API_KEY', 'E56342A9B75659AE55863C8B1F8EAC24');
+define('STEAM_DOMAIN_NAME', 'https://cstree.206xmcu.eu.org/');
+define('STEAM_LOGOUT_PAGE', '');
+define('STEAM_LOGIN_PAGE', '');
+
